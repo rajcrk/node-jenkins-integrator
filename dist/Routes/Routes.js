d2a55aca3ff0e11d9088958dab5bfae3133e01f6
Raj Kumar <rkumar@ztech.io>
2019-06-01 18:26:31 +0530

    Validation Added !

--- dist/Routes/Routes.js
+++ dist/Routes/Routes.js
@@ -13,21 +13,49 @@ const JenkinsOperation = __importStar(require("../JenkinsController/JenkinsOpera
  * Veiw all property page.
  */
 exports.startJenkinsJob = (req, res) => {
+    let isMessageValidBoolean;
     var resposeToSend;
-    let sMessage = formattingMessage(req);
-    let message = SplitString(sMessage);
-    console.log(`Message is ${message[0]} Message 2 is ${message[1]}, Message 3 is ${message[2]}`);
+    let chatBotMessage = formattingMessage(req);
+    /*
+    Checking if the user provided message is valid or not
+    */
+    // if (!isMessageValid(chatBotMessage)) { res.send({ text: `Semms like you typed in something wrong !, Dont worry follow this method\nZilker Deploy Bot, Mr. Meeseeks\nUsing me you can deploy projects by providing in the depolyment ID\nSounds fun right ?\nFormat for using this bot\nbuild <project-name> <work-area> <commit-id>` }) }
+    if (!isMessageValid(chatBotMessage)) {
+        res.send({
+            "cards": [
+                {
+                    "sections": [
+                        {
+                            "widgets": [
+                                {
+                                    "textParagraph": {
+                                        "text": "<b>Mr. Meeseeks</b>:<br>Seems like your message is not following the format<hr><br>Format for using this bot <b><font color=\"#0000ff\"><br>build [project-name] [work-area] [commit-id] </font></b>"
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        });
+        return;
+    }
+    let message = SplitString(chatBotMessage);
     var projectName = message[1];
     var workArea = message[2];
     var commitId = message[3];
+    console.log('Just before fetchAsync');
     JenkinsOperation.fetchAsync(projectName, workArea, commitId)
         .then(data => {
+        console.log('=================Entering into JenkinsOperation==========================');
         console.log(data);
         resposeToSend = data;
+        res.send({ text: `${projectName} setup started ...` });
     })
-        .catch(reason => console.log(reason.message));
-    console.log('Data Received is ', resposeToSend);
-    res.send({ text: `${projectName} Setup Started` });
+        .catch(reason => {
+        console.log(reason.message);
+        res.send({ text: `Seems Something Went Wrong !! ...` });
+    });
 };
 function SplitString(inputMessage) {
     let res = [];
@@ -38,7 +66,7 @@ function SplitString(inputMessage) {
 function formattingMessage(req) {
     let text = '';
     if (req.body.type === 'ADDED_TO_SPACE' && req.body.space.type === 'ROOM') {
-        text = `Thanks for adding me to ${req.body.space.displayName}`;
+        text = `Thanks for adding me to ${req.body.space.displayName}\nWelcome to the super awesome Zilker Deploy Bot, Mr. Meeseeks\nUsing me you can deploy projects by providing in the depolyment ID\nSounds fun right ?\nFormat for using this bot\nbuild <project-name> <work-area> <commit-id>`;
         // Case 2: When BOT was added to a DM
     }
     else if (req.body.type === 'ADDED_TO_SPACE'
@@ -50,5 +78,12 @@ function formattingMessage(req) {
         text = `${req.body.message.text}`;
     }
     return text;
+}
+function isMessageValid(message) {
+    let splittedMessage = message.split(" ");
+    if (splittedMessage.length != 4 && !(splittedMessage[2] == "ui" || splittedMessage[2] == "service")) {
+        return false;
+    }
+    return true;
 }
 //# sourceMappingURL=Routes.js.map
